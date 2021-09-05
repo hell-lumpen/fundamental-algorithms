4079cb6343b04c9ea0110a5f4e52cc660ab63126
Maxim <grubenko.m@ya.ru>
2021-09-05 16:20:11 +0300

    полностью готовое 1 задание

--- LAB1/task1/main.cpp
+++ LAB1/task1/main.cpp
@@ -1,42 +1,22 @@
 #include <iostream>
 #include <cstdarg>
+#include <fstream>
 
 int search_substr(const char* string, const char* substring);
+int search_substring_in_files(int (*fcnPtr)(const char*, const char*), const char* substr, int count_paths...);
+
 
 int main() {
-    std::cout << search_substr("12345678", "1") << std::endl;
-    int (*fcnPtr)(const char*, const char*) = search_substr;
-
-
+    search_substring_in_files(search_substr, "12", 4, "1.txt", "2.txt", "3.txt", "4.txt");
     return 0;
 }
 
-int sum(int n, ...)
-{
-    int result = 0;
-    va_list factor;         //указатель va_list
-    va_start(factor, n);    // устанавливаем указатель
-    for(int i=0;i<n; i++)
-    {
-        result += va_arg(factor, int);  // получаем значение текущего параметра типа int
-    }
-    va_end(factor); // завершаем обработку параметров
-    return result;
-}
-
-//// поиск подстроки в строке
-
 /*
-int boo();
-double doo();
-int moo(int a);
+char * p = (char*) malloc(10*sizeof(char));
+if (p==NULL) exit (1);
+p = (char *) realloc (p,15*sizeof(char));
 
-// Присваивание значений указателям на функции
-int (*fcnPtr1)() = boo; // ок
-int (*fcnPtr2)() = doo; // не ок: тип указателя и тип возврата функции не совпадают!
-double (*fcnPtr4)() = doo; // ок
-fcnPtr1 = moo; // не ок: fcnPtr1 не имеет параметров, но moo() имеет
-int (*fcnPtr3)(int) = moo; // ок
+free(p);
  */
 
 int search_substr(const char* string, const char* substring)
@@ -59,17 +39,30 @@ int search_substr(const char* string, const char* substring)
     return 0;
 }
 
-int search_substring_in_files(char* (*fcnPtr)(const char*, const char*), ...)
+int search_substring_in_files(int (*fcnPtr)(const char*, const char*), const char* substr, int count_paths...)
 {
     va_list file_paths;
-    va_start(file_paths, fcnPtr);
+    va_start(file_paths, count_paths);
 
-    int count_paths = 3;
     for (int i = 0; i < count_paths; i++)
     {
         char* path = va_arg(file_paths, char*);
-
+        std::ifstream inf(path);
+        if (!inf)
+        {
+            std::cerr << "Uh oh, could not be opened for reading!" << std::endl;
+            exit(1);
+        }
+        std::string buffer;
+        while(getline(inf, buffer))
+        {
+            if (fcnPtr(buffer.c_str(), substr))
+                std::cout << path << std::endl;
+        }
+        inf.close();
     }
+
+    va_end(file_paths);
     
     return 0;
 }
