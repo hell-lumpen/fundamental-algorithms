083a308f21e39e036f9d6a0e696ecd9a60a25d59
Maxim <grubenko.m@ya.ru>
2021-09-22 12:19:14 +0300

    готовое третье задание

--- LAB1/task3/main.cpp
+++ LAB1/task3/main.cpp
@@ -6,7 +6,7 @@
 int polynomial_value(int x, int n, ...);
 
 int main() {
-    polynomial_value(3, 2, 3, 2, 3);
+    polynomial_value(5, 2, 3, 2, 3);
     return 0;
 }
 
@@ -15,9 +15,8 @@ int polynomial_value(int x, int n, ...)
     va_list ratio;
     va_start(ratio, n);
     int result = 0;
-    for (int i = 0; i < n + 1; i++)
-    {
-        result += pow(x, n - i) * va_arg(ratio, int);
+    for (int i = 0; i < n + 1; i++) {
+        result = result * x + va_arg(ratio, int);
     }
     va_end(ratio);
     std::cout << result << std::endl;
