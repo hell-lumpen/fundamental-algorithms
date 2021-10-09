3196bf319eafa4c7edf7046ad67215a5647ddd16
Maxim <grubenko.m@ya.ru>
2021-10-09 16:26:39 +0300

    полностью готовое 1 задание

--- LAB1/task1/main.cpp
+++ LAB1/task1/main.cpp
@@ -1,38 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <cstdarg>
 #include <fstream>
 #include <string>
 
-int search_substr(const char* string, const char* substring);
-int search_substring_in_files(int (*fcnPtr)(const char*, const char*), const char* substr, int count_paths...);
-
-
-int main() {
-    search_substring_in_files(search_substr, "12", 4, "1.txt", "2.txt", "3.txt", "4.txt");
-    return 0;
-}
-
-int search_substr(const char* string, const char* substring)
-{
-    int i = 0, k = 0;
-    while (*(string + i) != '\0'){
-        if (*(string + i) == *(substring + k))
-        {
-            if ((*(substring + (++k)) == '\0'))
-                return 1;
+std::vector<int> search_substr(const std::string& s) {
+    std::vector<int> pi(s.length(), 0);
+    for (int i = 1; i < s.length(); i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) {
+            j = pi[j - 1];
         }
-        else
-        {
-            if ((*(substring + k) == '\0'))
-                return 1;
-            k = 0;
+        if (s[i] == s[j]) {
+            pi[i] = j + 1;
+        } else {
+            pi[i] = j;
         }
-        i++;
     }
-    return 0;
+    return pi;
 }
 
-int search_substring_in_files(int (*fcnPtr)(const char*, const char*), const char* substr, int count_paths...)
+int search_substring_in_files(std::vector<int> (*fcnPtr)(const std::string&), const char* substr, int count_paths...)
 {
     va_list file_paths;
     va_start(file_paths, count_paths);
@@ -51,11 +39,16 @@ int search_substring_in_files(int (*fcnPtr)(const char*, const char*), const cha
         }
 
         std::string buffer;
+        std::string substring(substr);
         while(getline(inf, buffer))
         {
-            if (fcnPtr(buffer.c_str(), substr)) {
-                file_names[k] = path;
-                k++;
+            std::vector<int> pi = fcnPtr(substring + "#" + buffer);
+            size_t substr_len = substring.length();
+
+            for (size_t j = 0; j < buffer.length(); j++) {
+                if (pi[substr_len + 1 + j] == substr_len) {
+                    std::cout << "string[" << j - substr_len + 1 << ".." << j << "] = substring in file: " << path << std::endl;
+                }
             }
         }
         inf.close();
@@ -66,6 +59,15 @@ int search_substring_in_files(int (*fcnPtr)(const char*, const char*), const cha
 
     delete [] file_names;
     va_end(file_paths);
-    
+
     return 0;
 }
+
+int main() {
+    search_substring_in_files(search_substr, "12", 4, "1.txt", "2.txt", "3.txt", "4.txt");
+
+    return 0;
+}
+
+
+
