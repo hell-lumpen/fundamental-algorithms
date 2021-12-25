5237cd519cfbed848570e436cd2a7d0f42b78a33
Grubenko Maxim <75304090+hell-lumpen@users.noreply.github.com>
2021-12-25 21:55:20 +0300

    Update main.cpp
    обновлено задание. теперь странных символов не появляется

--- LAB1/task5/main.cpp
+++ LAB1/task5/main.cpp
@@ -1,111 +1,87 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <cstdarg>
+#include <algorithm>
 
-class StringNumber{
-private:
-    char* str_number;
-    int base;
-public:
-    StringNumber(char* str, int base){
-        this->str_number = str;
-        this->base = base;
-    }
+using std::string;
+using std::cout;
+using std::endl;
 
-    StringNumber(int len, int base){
-        this->str_number = (char* ) malloc(sizeof(char) * (len));
-        memset(this->str_number, ' ', len);
-        this->str_number[len] = '\0';
-        this->base = base;
-    }
-    ~StringNumber(){
-        free(this->str_number);
-    }
-    StringNumber (const StringNumber &number){
-        size_t len = strlen(number.str_number);
-        this->str_number = (char* ) malloc(sizeof(char) * len);
-        for (size_t i = 0; i < len; ++i) {
-            this->str_number[i] = number.str_number[i];
-        }
-        this->base = number.base;
-    }
-
-    StringNumber operator = (const StringNumber &number){
-        size_t len = strlen(number.str_number);
-        this->str_number = (char* ) malloc(sizeof(char) * len);
-        for (size_t i = 0; i < len; ++i) {
-            this->str_number[i] = number.str_number[i];
-        }
-        this->base = number.base;
-        return *this;
-    }
-
-    friend StringNumber operator + (StringNumber &number1, const StringNumber &number2){
-
-        int len_n1 = std::strlen(number1.str_number);
-        int len_n2 = std::strlen(number2.str_number);
-        int len_n3 = (len_n1 > len_n2) ? len_n1 + 1: len_n2 + 1;
-
-        StringNumber number3(len_n3, number1.base);
-
-        int carry = 0;
-        for (int i = 1; i <= len_n3; i++) {
-            int last_digit_1 = (i > len_n1) ? 0 : (isdigit(number1.str_number[len_n1 - i]) ? number1.str_number[len_n1 - i] - '0' : toupper(number1.str_number[len_n1 - i]) - 'A' + 10);
-            int last_digit_2 = (i > len_n2) ? 0 : (isdigit(number2.str_number[len_n2 - i]) ? number2.str_number[len_n2 - i] - '0' : toupper(number2.str_number[len_n2 - i]) - 'A' + 10);
-            int last_res_digit = last_digit_1 + last_digit_2 + carry;
-            number3.str_number[len_n3 - i] = (last_res_digit % number1.base > 9) ? last_res_digit % number1.base + 'A' - 10 : last_res_digit % number1.base + '0';
-            carry = (last_res_digit / number1.base);
-        }
-
-        return number3;
-    }
-
-    friend std::ostream& operator<< (std::ostream &out, const StringNumber &number) {
-        int i = 0;
-        while (number.str_number[i] == '0')
-            i++;
-        for (; i < strlen(number.str_number); ++i) {
-                out << number.str_number[i];
-        }
-        out << "_" << number.base << std::endl;
-        return out;
-    }
-};
-
-void sum(const char* number1, const char* number2, int base) {
-    int len_n1 = strlen(number1);
-    int len_n2 = strlen(number2);
-    int len_n3 = (len_n1 >= len_n2) ? len_n1 + 1: len_n2 + 1;
-    char* number3 = (char* ) malloc(sizeof(char) * (len_n3));
-    memset(number3, ' ', len_n3);
-    number3[len_n3] = '\0';
-    int carry = 0;
-    for (int i = 1; i <= len_n3; i++) {
-        int last_digit_1 = (i > len_n1) ? 0 : (isdigit(number1[len_n1 - i]) ? number1[len_n1 - i] - '0' : toupper(number1[len_n1 - i]) - 'A' + 10);
-        int last_digit_2 = (i > len_n2) ? 0 : (isdigit(number2[len_n2 - i]) ? number2[len_n2 - i] - '0' : toupper(number2[len_n2 - i]) - 'A' + 10);
-        int last_res_digit = last_digit_1 + last_digit_2 + carry;
-        number3[len_n3 - i] = (last_res_digit % base > 9) ? last_res_digit % base + 'A' - 10 : last_res_digit % base + '0';
-        carry = (last_res_digit / base);
-    }
-    std::cout << number3 << std::endl;
-    free(number3);
+int _to_number(char character) {
+    if (character >= '0' && character <= '9') return (character - '0');
+    if (character >= 'a' && character <= 'z') return (character - 'a' + 10);
+    if (character >= 'A' && character <= 'Z') return (character - 'A' + 10);
+    return -1;
 }
 
-void add(StringNumber &n1, StringNumber &n2, int base, int count_number, ...){
-    va_list numbers;
-    va_start(numbers, count_number);
-    StringNumber result = n1 + n2;
-    for (int i = 0; i < count_number; i++) {
-        result = result + *va_arg(numbers, StringNumber*);
+char _to_char(int number) {
+    if (number >= 0 && number <= 9) return (number + '0');
+    else return (number + 'A' - 10);
+}
+
+void remove_leading_zeros(string &result) {
+    while(result.size() > 1 && result.back() == '0')
+        result.pop_back();
+}
+
+string sum(int base, string num_1, string num_2) {
+    reverse(num_1.begin(), num_1.end());
+    reverse(num_2.begin(), num_2.end());
+
+    string result;
+
+    int carry = 0;
+    size_t index_1 = 0, index_2 = 0;
+
+    for(; index_1 < num_1.length() && index_2 < num_2.length(); index_1++, index_2++) {
+        auto digit_1 = _to_number(num_1[index_1]);
+        auto digit_2 = _to_number(num_2[index_2]);
+        if (digit_1 == -1 || digit_2 == -1) return "ERROR:\tundefined character in number";
+        if (digit_1 + 1 > base || digit_2 + 1 > base) return "ERROR:\tincorrect number for the entered number system";
+        result += _to_char((digit_1 + digit_2 + carry) % base);
+        carry = (digit_1 + digit_2 + carry) / base;
     }
-    va_end(numbers);
-    std::cout << "----" << result << std::endl;
+
+    for(;index_1 < num_1.length(); index_1++) {
+        auto digit = _to_number(num_1[index_1]);
+        if (digit == -1) return "ERROR:\tundefined character in number";
+        if (digit + 1 > base) return "ERROR:\tincorrect number for the entered number system";
+        result += _to_char((digit + carry) % base);
+        carry = (digit + carry) / base;
+    }
+
+    for(;index_2 < num_2.length(); index_2++) {
+        auto digit = _to_number(num_2[index_2]);
+        if (digit == -1) return "ERROR:\tundefined character in number";
+        if (digit + 1 > base) return "ERROR:\tincorrect number for the entered number system";
+        result += _to_char((digit + carry) % base);
+        carry = (digit + carry) / base;
+    }
+
+    if (carry != 0) result += _to_char(carry);
+
+    remove_leading_zeros(result);
+
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+string total_amount(int number_base, int numbers_amount, ...)
+{
+    va_list args;
+    va_start(args, numbers_amount);
+    string result = "0";
+    if (number_base > 36) return string("ERROR:\tMax number number_base = 36!");
+    for (int i = 0; i < numbers_amount; ++i) {
+        result = sum(number_base, result, string(va_arg(args, char*)));
+    }
+    va_end(args);
+    return result;
 }
 
 int main() {
-    StringNumber C("111", 2);
-    StringNumber B("111", 2);
-    StringNumber A("111", 2);
-    add(A, B, 2, 1, &C);
+    cout << total_amount(16, 2, "AAAAA", "10z0000") << endl;
+    cout << total_amount(2, 3, "1000111", "10000000000000000000000000000000000000000001", "1") << endl;
+    cout << total_amount(36, 3, "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZz", "11111111111111111111111111111111111111111111111111111", "0") << endl;
     return 0;
 }
